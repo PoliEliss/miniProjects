514883f7206d9aebcba219bcb9e3a4cad6d13034
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-03-09 00:23:37 +0300

    add fun getResult

--- app/src/main/java/com/rorono/a0603thefirstproject/ProjectOneCounter/Calculate.kt
+++ app/src/main/java/com/rorono/a0603thefirstproject/ProjectOneCounter/Calculate.kt
@@ -2,22 +2,80 @@ package com.rorono.a0603thefirstproject.ProjectOneCounter
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.rorono.a0603thefirstproject.R
+import net.objecthunter.exp4j.ExpressionBuilder
 
-class Calculate: AppCompatActivity() {
+class Calculate : AppCompatActivity() {
+    var tvInput: TextView? = null
 
+    var lastNumeric: Boolean = false
+
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_3)
+        tvInput = findViewById(R.id.fild)
+    }
+
+    fun getNumber(view: View) {
+        //Toast.makeText(this, "Buttom clicked", Toast.LENGTH_LONG).show()
+
+        tvInput?.append((view as Button).text)
+
+        lastNumeric = true
+        lastDot = false
 
     }
 
-    fun getNumber(view:View){
-        Toast.makeText(this,"Buttom clicked", Toast.LENGTH_LONG).show()
+
+    fun ocClear(view: View) {
+        tvInput?.text = ""
+    }
+
+    fun delereElementEnd(view: View) {
+        tvInput?.text = tvInput?.text!!.substring(0, tvInput?.length()!! - 1)
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+
+
+
+
+   /* fun isAdd(value: String): Boolean {
+        return if (value.startsWith("-")){
+            false
+        } else {
+            value.contains("/")||value.contains("+") || value.contains("*")||value.contains("-")
+        }
+    }*/
+
+    fun getResult(view: android.view.View) {
+
+        val ex = ExpressionBuilder(tvInput?.text.toString()).build()
+        val result =  ex.evaluate()
+
+        var longRes = result.toLong()
+
+        if (result == longRes.toDouble()){
+            tvInput?.text = longRes.toString()
+        } else{
+            tvInput?.text = result.toString()
+        }
+
+
+
 
     }
 }
--- app/src/main/java/com/rorono/a0603thefirstproject/ProjectOneCounter/KotlinBasics.kt
+++ app/src/main/java/com/rorono/a0603thefirstproject/ProjectOneCounter/KotlinBasics.kt
@@ -2,30 +2,14 @@ package com.rorono.a0603thefirstproject.ProjectOneCounter
 
 fun main(){
 
-val nyName = " Frank"
-
-    print("Hello$nyName")
 
 
-    var feltTemp = " cold"
 
-    var roomTemp = 10
+    var name:String = "Nana"
 
+   println( name.substring(0,name.length-1))
 
-    print(feltTemp)
-    print(roomTemp)
-
-    val nana = Person("mmmm",90)
-    val test = Name()
 
 
 }
 
-class Person(name:String, age:Int)
-
-class Name (){
-
-    constructor( name:String):this(){
-
-    }
-}
